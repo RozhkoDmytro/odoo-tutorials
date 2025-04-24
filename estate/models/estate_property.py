d1e2df7c193a1f421147ebd8c7af57a06c252fc9
Dmytro Rozhko <d.rozhko.ua@gmail.com>
2025-04-24 12:07:47 +0300

    [IMP] view, valid data, defualt value

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,23 @@
 from odoo import models, fields  # type: ignore
+from dateutil.relativedelta import relativedelta  # type: ignore
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(string="Title", required=True, default="New unknown")
+    description = fields.Text(copy=False)
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(
+        string="Available From",
+        copy=False,
+        default=lambda self: fields.Date.today() + relativedelta(months=3),
+    )
+    active = fields.Boolean(string="Active", default=True)
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price", readonly=True)
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
@@ -23,4 +30,17 @@ class EstateProperty(models.Model):
             ("east", "East"),
             ("west", "West"),
         ],
+    )
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="new",
     )
